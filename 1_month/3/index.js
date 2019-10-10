465e320bd68af7f0eefdba76894a9a2d1f0efd5c
joekinzu <34208764+joekinzu@users.noreply.github.com>
2019-10-10 11:56:41 +0400

    hw3 done

--- 1_month/3/index.js
+++ 1_month/3/index.js
@@ -1,1 +1,27 @@
-// start
+const getPath = (HTMLElement) => {
+    // строим путь до css элемента(массив)
+    // первое значение элемента текущий html
+    let path = [];
+    // входной HTML код
+    // console.log(HTMLElement);
+    // поехале
+    while (
+        // чекаем на null
+        (HTMLElement !== null) &&
+        // чекаем на body(верхний элемент документа)
+        (HTMLElement.nodeName.toLowerCase() != 'body') && 
+        // пишем HTML в начало массива
+        // тэг 
+        path.unshift(HTMLElement.nodeName.toLowerCase() +
+		    // считаем вложенность
+		    (HTMLElement.childElementCount > 0 ? ':nth-child('+(Array.from(HTMLElement.parentNode.children).indexOf(HTMLElement)+1)+')' : '') + 
+        // id
+        (HTMLElement.id ? '#' + HTMLElement.id : '') + 
+        // класс
+        (HTMLElement.className ? '.' + HTMLElement.className.replace(/\s+/g, ".") : '')) &&
+        // присваем родителя
+        (HTMLElement = HTMLElement.parentNode)
+    );
+    // склеиваем массив в строку с разделителем >
+    return path.join(" > ");
+}
