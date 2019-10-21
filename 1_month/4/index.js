ad906224bd20051d9a9e947a363c3a5f01f2e84b
joekinzu <34208764+joekinzu@users.noreply.github.com>
2019-10-21 19:21:29 +0400

    update hw4

--- 1_month/4/index.js
+++ 1_month/4/index.js
@@ -1,44 +1,48 @@
-// компонент потомка
-customElements.define('my-leaf',
-    class extends HTMLElement {
-        constructor() {
-            super()
-        }
-        // рендер
-        connectedCallback() {
-            this.innerHTML = `${this.dataset.id} level<br>`
-            if(this.dataset.leaf){
-                const leaf = JSON.parse(this.dataset.leaf);
-                this.drawfunc(leaf)
-            }    
-        }
-        // функция отрисовки потомков 
-        drawfunc(leaf) {
-            console.log((leaf))
-            leaf.items ?
-                this.innerHTML += `<my-leaf data-id=${leaf.id} data-leaf=${JSON.stringify(leaf.items[0])}></my-leaf>` :
-                this.innerHTML += `<my-leaf data-id=${leaf.id} ></my-leaf>`
-        }    
-    }
-);
-// родительский компонент
+// Root Element
 customElements.define('my-tree',
     class extends HTMLElement {
         constructor() {
             super()
             this.attachShadow({ mode: 'open' })
         }
-        // рендер
+        // Render
         connectedCallback() {
             this.drawfunc(JSON.parse('{"id": 1,"items": [{"id": 2,"items": [{ "id": 3, "items": [{ "id": 4}]}]}]}'))
         }
-        // чекаем обьект на id, затем рекурсивно вызываем класс потомка
         drawfunc(data) {
-            console.log(data)
+            console.log('given tree:',data)
             if (data.id) {
-                // пробрасываем первый уровень
-                this.shadowRoot.innerHTML = `<my-leaf data-id=${data.id} data-leaf=${JSON.stringify(data.items[0])}></my-leaf>`
+                this.shadowRoot.innerHTML = `<my-leaf data-id=${data.id} data-leaf=${JSON.stringify(data.items)}></my-leaf>`
             }    
         }
     }
 );
+// Child Element
+customElements.define('my-leaf',
+    class extends HTMLElement {
+        constructor() {
+            super()
+        }
+        // Insert  Child into DOM
+        connectedCallback() {
+            this.innerHTML = `${this.dataset.id} level<br>`
+            this.dataset.leaf ?
+                Object.values(JSON.parse(this.dataset.leaf)).forEach(e => {
+                    if(e){
+                        const leaf = e
+                        this.drawfunc(leaf)
+                    } 
+                } 
+            ):
+            console.log('end')    
+        }
+        // Renderind child 
+        drawfunc(leaf) {
+            console.log((leaf))
+            leaf.items ?
+                this.innerHTML += `<my-leaf data-id=${leaf.id} data-leaf=${JSON.stringify(leaf.items)}></my-leaf>` :
+                this.innerHTML += `<my-leaf data-id=${leaf.id} ></my-leaf>`
+        }    
+    }
+);
+
