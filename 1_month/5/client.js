6ad3bf38bd900b9c2147de231f4d8d75f960f261
joekinzu <34208764+joekinzu@users.noreply.github.com>
2019-10-23 13:09:22 +0400

    hw5 done

--- 1_month/5/client.js
+++ 1_month/5/client.js
@@ -1,1 +1,50 @@
-// code
+// REQUEST BY HTTP
+const http = require('http');
+// ARGs PARSER
+const argv = require('minimist')(process.argv.slice(2));
+// -t parallell/serial
+// -n quantity
+let typeOfRequests = argv['t'];
+let quantytyOfRequests = argv['n'];
+
+// Variables
+const request = (c) => {
+  return new Promise((resolve, reject) => {
+    http
+      .get('http://localhost:3000/id='+c, res => {
+        let content = '';
+        res.on('data', chunk => {
+          content += chunk;
+        }),
+          res.on('end', () => {
+            resolve(content);
+          });
+      })
+      .on('error', err => reject(console.log('ERROR', err)));
+  });
+};
+
+const requestByType = async(quantytyOfRequests, typeOfRequests) => {
+  switch(typeOfRequests){
+    case 'parallel': 
+        const promises = []
+        for(let i=1;i<=quantytyOfRequests;i++){
+            promises.push(request(i))
+        }
+        Promise.all(promises)
+        .then(result =>console.log(result))
+        .catch(err => console.log(err))
+        break
+    case 'serial': 
+        for(let i=1;i<=quantytyOfRequests;i++){
+        await request(i)
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+        }
+        break
+    default:
+        console.log('Request type not provided')    
+  }
+};
+
+requestByType(quantytyOfRequests, typeOfRequests);
