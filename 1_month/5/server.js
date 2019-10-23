19f87d95189eca2a268cc605f3cbea1e39bfa04e
joekinzu <34208764+joekinzu@users.noreply.github.com>
2019-10-23 13:08:35 +0400

    hw5 done

--- 1_month/5/server.js
+++ 1_month/5/server.js
@@ -1,1 +1,21 @@
-// code
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const resmaker = () => {
+    if (req) {
+      console.log(`Request ${TimeMarker}, ID: ${id}`);
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello World, request time ${TimeMarker},ID: ${id}`);
+  };
+  let TimeMarker = new Date();
+  let id = req.url.split('=')[1];
+  setTimeout(() => resmaker(), 100);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
