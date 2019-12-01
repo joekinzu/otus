c3d9b3dce7d57d35e3cfeffbe0ef85fde1bb621d
unknown <deadassshit@ya.ru>
2019-12-01 19:50:05 +0400

    fsplit update

--- 2_month/3/fsplit.js
+++ 2_month/3/fsplit.js
@@ -1,31 +1,20 @@
 // Этап 1 
 const fs = require('fs')
 
-const stream = fs.createReadStream('data.txt', {
-    encoding: 'utf8',
-    fd: null,
-});
-const wstream1 = fs.createWriteStream('data1.txt')
-const wstream2 = fs.createWriteStream('data2.txt')
-const wstream3 = fs.createWriteStream('data3.txt')
-const wstream4 = fs.createWriteStream('data4.txt')
-let i = 1
+const stream = fs.createReadStream('data.txt', {encoding: 'utf8', fd: null,})
+
+const num = 20
+let i = Math.floor(Math.random()*num+1)
+let wstream = [];
+
+[...Array(num)].map((item,index) => {
+        wstream.push(fs.createWriteStream('data'+(num-index)+'.txt'))
+    })
 
 stream.on('readable', (chunk) => {
-    while (null !== (chunk = stream.read(100))) {
-        if (i===1){
-            wstream1.write(chunk)
-        }
-        if (i===2){
-            wstream2.write(chunk)
-        }
-        if (i===3){
-            wstream3.write(chunk)
-        }
-        if (i===4){
-            wstream4.write(chunk)
-        }
-        i = Math.floor(Math.random()*4)+1
+    while(chunk = stream.read(100)){
+        wstream[i-1].write(chunk)
+        i = Math.floor(Math.random()*num+1)
     }
   })
 
