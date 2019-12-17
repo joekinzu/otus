771fa4ec6546e0af67ff162fc8d15784a5a15b1e
unknown <deadassshit@ya.ru>
2019-12-17 23:56:10 +0400

    index updated

--- 2_month/3/index.js
+++ 2_month/3/index.js
@@ -2,38 +2,36 @@ const fs = require('fs')
 const readline = require("readline")
 
 // temp data
-let s = ''
+let a = []
+let b = []
+let end, begin
 // files count
 let wcount = 0
 // file size in mb
-const filesize = 10*1024*1024
+const filesize = 50*1024*1024
 // source file
-const rstream= fs.createReadStream('data.txt',)
+const rstream= fs.createReadStream('data.txt',{highWaterMark: filesize})
 const rstream_arr = []
 let lineReader = [];
 
 rstream.on('data', (chunk) => {
-  s = s + chunk
-  if(s.length > filesize){
-    rstream.pause()
-    wcount++
-    const wstream = fs.createWriteStream('data'+wcount+'.txt')
-    for(let j=0;j<10;j++){
-      re = new RegExp(j, 'g')
-      wstream.write(s.match(re).join('')+'\n')
-    }
-    s = ''
-    rstream.resume()
+  a.push(chunk.sort())
+  wcount++
+  console.log(wcount,' filesize - ',a[0].indexOf('1'))
+  const wstream = fs.createWriteStream('data'+wcount+'.txt')
+  for(let j=0;j<10;j++){
+    begin = a[0].indexOf((j).toString())
+    j!==9 ? end = a[0].indexOf((j+1).toString()) : end = a[0].length
+    let buf = Buffer.allocUnsafe(end-begin)
+    a[0].copy(buf,0,begin,end)
+    b.push(buf)
+    wstream.write(buf+'\n')
+    b = []
   }
+  a = []
 })
 
 rstream.on('end', () => {
-  wcount++
-  const wstream = fs.createWriteStream('data'+wcount+'.txt')
-  for(let j=0;j<10;j++){
-    re = new RegExp(j, 'g')
-    wstream.write(s.match(re).join('')+'\n')
-  }
   const wstream_sort = fs.createWriteStream('datasorted.txt')
   for(let i=0;i<wcount;i++){
     rstream_arr.push('data'+(i+1)+'.txt')
@@ -49,5 +47,5 @@ rstream.on('end', () => {
         wstream_sort.write(line)
     })
   }
-console.log(wcount, ' files created')
+  console.log(wcount, ' files created')
 })
